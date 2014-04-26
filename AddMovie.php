9bf21e3979b83964417fb2483018f9dcfd645c81
Doug Steiert <djsg38@mst.edu>
2014-04-25 22:05:22 -0500

    attempt tocommit

---
+++ AddMovie.php
@@ -0,0 +1,39 @@
+<?php
+
+$con = mysql_connect("localhost","root@localhost","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+  
+
+ 
+mysql_select_db("Flixnet", $con);
+
+
+
+if ($_POST['Title']!=""){
+	$sql="INSERT INTO Movie (MName, Genre, Rating, CityFilm, CountryFilm,
+	Weekday, MonthYear, YearMade, Synopsis)
+	VALUES
+	('$_POST[Title]','$_POST[Genre]','$_POST[Rating]','$_POST[CityFilmed]',
+	'&_POST[CountryFilmed]','&_POST[ReleaseDay]','&_POST[ReleaseMonth]',
+	'&_POST[ReleaseYear]','&_POST[Synopsis]')";
+	
+	if (!mysql_query($sql,$con))
+	  {
+	  die('Error: ' . mysql_error());
+	  }
+	echo $_POST['Title']." is added";
+	}
+else echo "The provided information is not correct!";
+
+mysql_close($con)
+
+?>
+<html>
+
+<br />
+<a href="Home.html">Back</a>
+
+</html>
