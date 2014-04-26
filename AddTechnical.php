ecb8256239118a9c7e3b06e26850f9bb0c7eeab2
Doug Steiert <djsg38@mst.edu>
2014-04-25 22:17:20 -0500

    Hello

---
+++ AddTechnical.php
@@ -0,0 +1,36 @@
+<?php
+
+$con = mysql_connect("localhost","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+  
+
+
+mysql_select_db("Flixnet", $con);
+
+
+
+if ($_POST['Movie']!=""){
+	$sql="INSERT INTO Technical (Mname, Runtime, Sound, Aspect)
+	VALUES
+	('&_POST[Movie]','$_POST[Runtime]','$_POST[Sound]','$_POST[Aspect]')";
+	
+	if (!mysql_query($sql,$con))
+	  {
+	  die('Error: ' . mysql_error());
+	  }
+	echo $_POST['Movie']." is added";
+	}
+else echo "The provided information is not correct!";
+
+mysql_close($con)
+
+?>
+<html>
+
+<br />
+<a href="Home.html">Back</a>
+
+</html>
