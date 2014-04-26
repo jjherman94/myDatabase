508e9288455a9aa2d149502a230cab7dc20c5cc5
Doug Steiert <djsg38@mst.edu>
2014-04-26 15:58:24 -0500

    Adding deletion of company

---
+++ DeleteCompany.php
@@ -0,0 +1,36 @@
+<?php
+
+$con = mysql_connect("localhost","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+  
+
+
+mysql_select_db("Flixnet", $con);
+
+
+
+if ($_POST['Company']!=""){
+	$sql="DELETE FROM Company
+	Where
+	(CName = '$_POST[Company]')";
+	
+	if (!mysql_query($sql,$con))
+	  {
+	  die('Error: ' . mysql_error());
+	  }
+	echo $_POST['Company']." has been deleted.";
+	}
+else echo "The provided information is not correct!";
+
+mysql_close($con)
+
+?>
+<html>
+
+<br />
+<a href="Home.html">Back</a>
+
+</html>
