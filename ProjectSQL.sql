a4892e5a472c3ea00efd1c4a7ce83fa72355fd33
Doug Steiert <djsg38@mst.edu>
2014-04-27 17:25:22 -0500

    Adding SQL file

---
+++ ProjectSQL.sql
@@ -0,0 +1,50 @@
+Create Table Company(
+  CName varchar(20) Primary Key REFERENCES Technical(Cname),
+  City varchar(15),
+  Country varchar(20)
+);
+
+Create Table Technical(
+  Cname varchar(20) Not Null References Company(CName),
+  Mname varchar(30) Not Null References Movie(MName),
+  Runtime varchar(10) Primary Key,
+  Sound varchar(15),
+  Aspect varchar(10)
+);
+
+Create Table BoxOffice(
+  Mname2 varchar(30) Not Null References Movie(MName),
+  DayofWeek varchar(10),
+  MonthofYear varchar(10),
+  YearRelease integer(4) Not Null,
+  Budget varchar(15),
+  GrossIncome varchar(20) Not Null,
+  Primary Key(DayofWeek, MonthofYear, YearRelease)
+);
+
+Create Table ActorActress(
+  AName varchar(30) Primary Key,
+  NumberMovies int
+);
+
+Create Table MoviesIn(
+  AName varchar(30) References ActorActress(AName),
+  Movies varchar(30)
+);
+
+Create Table Movie(
+  MName varchar(30) Primary Key,
+  Rating varchar(5) Not Null,
+  Synopsis varchar(250),
+  Weekday varchar(10),
+  MonthYear varchar(10),
+  YearMade integer(4) Not Null,
+  CountryFilm varchar(20),
+  CityFilm varchar(20)
+);
+
+Create Table Genre(
+  MName varchar(30),
+  Genres varchar(15) Not Null,
+  Foreign Key (MName) REFERENCES Movie(MName)
+);
