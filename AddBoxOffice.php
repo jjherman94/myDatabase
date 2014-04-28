d14fff9000a903c786934c692dd822d11972edea
jjherman94 <jjhf39@mst.edu>
2014-04-27 20:57:53 -0500

    fix DOUGS MISTAKE!!

--- AddBoxOffice.php
+++ AddBoxOffice.php
@@ -19,8 +19,8 @@ if ($_POST['Title']!=""){
 	$sql="INSERT INTO BoxOffice (Mname2, DayofWeek, MonthofYear, YearRelease,
         Budget, GrossIncome)
 	VALUES
-	('$_POST[Title]','$_POST[Day]','$_POST[Month]','&_POST[Year]',
-        '&_POST[Budget]','&_POST[GrossIncome]')";
+	('$_POST[Title]','$_POST[Day]','$_POST[Month]','$_POST[Year]',
+        '$_POST[Budget]','$_POST[GrossIncome]')";
 	
 	if (!mysql_query($sql,$con))
 	  {
